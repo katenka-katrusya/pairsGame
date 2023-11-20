2a0c980ea6afa6f61abde5f0baff4a4db07241ae
SEkaterina <katerina-kozyrenko@yandex.ru>
2023-11-20 13:25:14 +0300

    upd

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const container = document.querySelector('.container');
+let interval;
 
 // Этап 1. Создайте функцию, генерирующую массив парных чисел. Пример массива, который должна возвратить функция:
 // [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]. count - количество пар.
 
 function createNumbersArray(count) {
   let arrPairedNumbers = [];
-  for (let i = 1; i <= count; i++) {
+  for (let i = 1; i <= count * count / 2; i++) {
     arrPairedNumbers.push(i, i);
   }
   return arrPairedNumbers;
@@ -31,29 +32,44 @@ function shuffle(array) {
 function startGame(count) {
   const listItems = document.createElement('ul');
   const restartButton = document.createElement('button');
+  const timer = document.createElement('div');
 
   const cards = [];       // массив карточек
-  let flippedCards = [];  // массив для перевёрнутых карточек (обнуляется)
+  let flippedCards = [];  // массив для перевёрнутых карточек двух (для сравнения) (обнуляется)
 
   const shuffledNumbers = shuffle(createNumbersArray(count));
 
+  timer.textContent = '00:01:00';
   restartButton.textContent = 'Начать заново';
 
+  timer.classList.add('mb-3');
   listItems.classList.add('row', 'g-2', 'list-unstyled', 'justify-content-center');
   restartButton.classList.add('btn', 'btn-secondary');
 
   switch (count) {
     case 2:
       listItems.classList.add('row-cols-2');
-      container.style.width = '250px';
+      container.style.maxWidth = '250px';
       break;
     case 4:
-    case 6:
-    case 8:
       listItems.classList.add('row-cols-4');
+      container.style.maxWidth = '500px';
+      break;
+    case 6:
+      listItems.classList.add('row-cols-6');
+      container.style.maxWidth = '700px';
+      break;
+    case 8:
+      listItems.classList.add('row-cols-8');
+      container.style.maxWidth = '900px';
+      break;
+    case 10:
+      listItems.classList.add('row-cols-10');
+      container.style.maxWidth = '1100px';
       break;
   }
 
+  container.append(timer);
   container.append(listItems);
   container.append(restartButton);
 
@@ -80,7 +96,6 @@ function startGame(count) {
     restartButton.addEventListener('click', () => newGame(count, cards));
   }
 
-
   function flipCard(card, number) {
     if (card.classList.contains('is-flipped') || flippedCards.length === 2) {
       return; // выходим и не выполняем дальнейшие действия
@@ -89,7 +104,7 @@ function startGame(count) {
       flippedCards.push({card, number});
     }
 
-    if (flippedCards.length === 2) {      //   проверка на совпадение значений карточек
+    if (flippedCards.length === 2) {            // проверка на совпадение значений карточек
       const firstCard = flippedCards[0].number;
       const secondCard = flippedCards[1].number;
 
@@ -104,12 +119,28 @@ function startGame(count) {
         }, 1000);
       }
     }
+    // если все карточки открыты, то игра окончена
+    if (cards.every((item) => item.classList.contains('is-flipped'))) {
+      setTimeout(() => {
+        gameOver(timer);
+      }, 1000);
+    }
   }
 
-  setTimeout(() => {
-    newGame(count, cards);
-    alert('Время вышло!');
-  }, 60000);
+  StartTimer(timer);
+}
+
+function StartTimer(timer) {
+  let seconds = 60;
+
+  interval = setInterval(() => {
+    seconds > 0 ? seconds-- : clearInterval(interval);
+    timer.textContent = `00:00:${seconds.toString().padStart(2, '0')}`; // добавляем 0 в начале, если длина строки < 2
+    console.log('timer.textContent:', timer.textContent);
+    if (timer.textContent === '00:00:00') {
+      gameOver(timer);
+    }
+  }, 1000);
 }
 
 function newGame(count, cards) {
@@ -121,6 +152,37 @@ function newGame(count, cards) {
     container.innerHTML = '';
     startGame(count);
   }, 800);
+}
+
+function gameOver(timer) {
+  const div = document.createElement('div');
+  const newGameButton = document.createElement('button');
+
+  clearInterval(interval); // останавливаем таймер, чтобы он не шёл, когда карты все открыты
+
+  const timeLeft = timer.textContent.split(':').at(-1);   // разбираем строку и забираем последний элемент после :
+
+  if (timer.textContent === '00:00:00') {
+    div.textContent = 'Игра окончена, время вышло!';
+  } else {
+    div.textContent = `Игра окончена! Вы закончили игру за ${60 - timeLeft} секунд!`;
+  }
+
+  newGameButton.textContent = 'Сыграть ещё';
+
+  div.classList.add('mb-3');
+  newGameButton.classList.add('btn', 'btn-primary');
+
+  container.innerHTML = '';
+
+  container.append(div);
+  container.append(newGameButton);
+
+  newGameButton.addEventListener('click', () => {
+    container.innerHTML = '';
+    container.style.maxWidth = '';
+    PlayingField();
+  });
 }
 
 // Выбор игрового поля перед стартом игры
